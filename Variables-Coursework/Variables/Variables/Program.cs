aac55b337259f22c6c7dea4658ff1ca6137763e5
Bradley Wilson <bradleywilson727@gmail.com>
2022-05-31 12:25:39 -0500

    completed assignment

---
+++ Variables-Coursework/Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Bradley Wilson";
+
+            int myAge = 34;
+
+            char x = 'y';
+
+            bool havingFun = true;
+
+            double pi = 3.14;
+
+            decimal almostPi = 3.1m;
+
+            Console.WriteLine($"My name is {myName} and I am {myAge} years old.");
+
+        }
+    }
+}
